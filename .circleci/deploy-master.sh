5e6af5456923d145f2cd6ccb74ed14b6e532e3a9
Yvonne Yao <65931215+afaryy@users.noreply.github.com>
2020-06-07 22:27:11 +1000

    Add deploy-master file
    Download latest binary from release to EC2.

---
+++ .circleci/deploy-master.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+user=ec2-user
+host=54.66.230.45
+src=`pwd`"/"
+des=/opt/gocode
+now=`date +"%Y-%m-%d %H:%M:%S"`
+#ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no  ec2-user@54.66.230.45 "sudo ls /opt/"
+#sync -vzrc --delete  --exclude ".git"  --exclude ".env"   --exclude ".circleci"   $src  $user@$host:$des
+
+#ssh $user@$host "sudo chown -R ec2-user:ec2-user $des"
+
+#ssh $user@$host "chmod -R 775 $des/bootstrap/cache && chmod -R 775 $des/storage && cd $des && pwd && ls -al"
+
+echo "$now update $host $des code"
+
+echo "部署完毕"
